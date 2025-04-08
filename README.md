eba4610f1438017199112ddb2e53d77e71f141f2
g_rocket <georgiiromanishko@gmail.com>
2025-04-08 13:04:59 +0200

    feat(project): init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Cpp
+
+```c++
+#include <iostream>
+using namespace std;
+```
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <memory> 
+using namespace std;
+
+int main() {
+    return 0;
+}
