ed53a2c582f27621aba2fc5a635d3fbbc2c353f9
g_rocket <georgiiromanishko@gmail.com>
2025-04-10 14:02:25 +0200

    feat(project): mutex, sync streams

--- README.md
+++ README.md
@@ -226,3 +226,49 @@ int main() {
     return 0;
 }
 ```
+
+## Mutex, sync streams
+
+```c++
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+mutex mtx;
+
+void print(char ch) 
+{
+    this_thread::sleep_for(chrono::milliseconds(2000));
+
+    mtx.lock();
+
+    for(int i = 0; i < 5; i++) 
+    {
+        for(int j = 0; j < 10; j++) 
+        {
+            cout << ch;
+
+            this_thread::sleep_for(chrono::milliseconds(20));
+        }
+
+        cout << endl;
+    }
+
+    mtx.unlock();
+
+    this_thread::sleep_for(chrono::milliseconds(2000));
+}
+
+
+int main() {
+    thread t1(print, '-');
+    thread t2(print, '*');
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
+```
--- main.cpp
+++ main.cpp
@@ -1,67 +1,41 @@
 #include <iostream>
-#include <string>
-#include <memory> 
+#include <mutex>
 #include <thread>
 #include <chrono>
 using namespace std;
 
+mutex mtx;
 
-class MyClass {
-    public:
-        void DoWork() {
-            this_thread::sleep_for(chrono::milliseconds(2000));
-            cout << "Id stream: " << this_thread::get_id() << " -----\t" << "DoWork started\t-----" << endl; 
+void print(char ch) 
+{
+    this_thread::sleep_for(chrono::milliseconds(2000));
 
-            this_thread::sleep_for(chrono::milliseconds(5000));
-            cout << "Id stream: " << this_thread::get_id() << " -----\t" << "DoWork stoped\t-----" << endl; 
+    mtx.lock();
+
+    for(int i = 0; i < 5; i++) 
+    {
+        for(int j = 0; j < 10; j++) 
+        {
+            cout << ch;
+
+            this_thread::sleep_for(chrono::milliseconds(20));
         }
 
-        void DoWork2(int a) {
-            this_thread::sleep_for(chrono::milliseconds(2000));
-            cout << "Id stream: " << this_thread::get_id() << " -----\t" << "DoWork 2 started\t-----" << endl; 
-
-            this_thread::sleep_for(chrono::milliseconds(5000));
-            cout << "DoWork 2 value:\t" << a << endl;
-
-            cout << "Id stream: " << this_thread::get_id() << " -----\t" << "DoWork 2 stoped\t-----" << endl; 
-        }
-
-        int Sum(int a, int b) {
-            this_thread::sleep_for(chrono::milliseconds(2000));
-            cout << "Id stream: " << this_thread::get_id() << " -----\t" << "Sum started\t-----" << endl; 
-
-            this_thread::sleep_for(chrono::milliseconds(5000));
-
-            cout << "Id stream: " << this_thread::get_id() << " -----\t" << "Sum stoped\t-----" << endl; 
-    
-            return a + b;
-        }
-};
-
-int main() {
-    int result;
-    MyClass m;
-
-    thread t(&MyClass::DoWork, m);
-
-    thread t1(&MyClass::DoWork2, m, 10);
-
-    thread t2([&result, &m]() {
-        result = m.Sum(2, 5);
-    });
-
-
-    for(size_t i = 1; i <= 10; i++) {
-        cout << "Id stream = " << this_thread::get_id() << "\tMain\t" << i << endl;
-
-        this_thread::sleep_for(chrono::milliseconds(500));
+        cout << endl;
     }
 
-    t.join();
+    mtx.unlock();
+
+    this_thread::sleep_for(chrono::milliseconds(2000));
+}
+
+
+int main() {
+    thread t1(print, '-');
+    thread t2(print, '*');
+
     t1.join();
     t2.join();
-
-    cout << "Result\t" << result << endl;
 
     return 0;
 }
